c82cf9641366844eb6c3f1653268ac36915a22b5
thaynasantosss <thayna.2015.09@gmail.com>
2023-05-19 22:39:25 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,261 @@
+﻿Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1650, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(2200, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(2200, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1650, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1650, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1320, 500);
+
+
+
+Thread.Sleep(500);
+
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(2200, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1650, 500);
+
+
+
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1485, 500);
+
+Console.Beep(1650, 500);
+
+
+
+
+Thread.Sleep(1000);
+
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.Beep(1650, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1320, 500);
+
+Console.Beep(1650, 500);
+
+Console.Beep(1980, 500);
+
+Console.Beep(1980, 500);
+
+
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+Console.WriteLine("MI, MI, MI, DÓ, MI, SOL, SOL,");
+Console.WriteLine("DO-SOL-MI-LA-SI-LA-SOL");
+Console.WriteLine("DO-DO-SOL-LA-MI-SOL-MI-DO-RE-SOL");
+Console.WriteLine("DO-DO-MI-SOL-DO - DO-DO-RE-DO-RE-MI");
+Console.WriteLine("DO-DO-MI-SOL-DO-SOL-SOL-SOL");
+Console.WriteLine("DO-DO-MI-SOL-DO - DO-DO-RE-DO-RE-MI ");
+Console.WriteLine("MI SOL DO");
+Console.WriteLine("DO DO DO DO RE MI DO LÁ SOL");
+Console.WriteLine("DO DO DO DO RE MI");
+Console.WriteLine("DO DO DO DO RE MI");
+Console.WriteLine("MI MI MI DO MI SOL SOL");
+
+
+
+//FALTA ARRUMARRRRRRRRR
+
+//                                                                Dó	1320
+//                                                                Ré	1485
+//                                                                Mi	1650
+//                                                                Fá	1759
+//                                                                Sol	1980
+//                                                                Lá	2200
+//                                                                Si	2475
+
+
